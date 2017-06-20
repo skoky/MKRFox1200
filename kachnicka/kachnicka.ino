54b340bdca2c3f5575e3cb86ddb19691cbb7aa7b
Ladislav Skokan <lskokan@cisco.com>
2017-06-20 22:06:46 +0200

    adding sensor 18ds20

--- kachnicka/kachnicka.ino
+++ kachnicka/kachnicka.ino
@@ -1,7 +1,13 @@
 #include <ArduinoLowPower.h>
 #include <SigFox.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-int sleepMins = 20;
+#define ONE_WIRE_BUS 2
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+int sleepMins = 15;
 
 #define DEBUG 0
 
@@ -12,6 +18,7 @@ void setup() {
     Serial.println("Shield error or not present!");
     return;
   }
+  sensors.begin();
 
   if (DEBUG) {
     Serial.begin(9600);
@@ -22,7 +29,19 @@ void setup() {
 
 void loop() {
 
-  sendString();
+  if (DEBUG) {
+    Serial.println(" Requesting temperatures...");
+  }
+
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  float temp = sensors.getTempCByIndex(0);
+
+  if (DEBUG) {
+    Serial.print("Temperature is: ");
+    Serial.println(temp); // Why "byIndex"?
+  }
+
+  sendTemp(temp);
 
   if (DEBUG) {
     Serial.print("Sleeping now minutes: ");
@@ -30,13 +49,13 @@ void loop() {
   }
   // LowPower.sleep(sleepMins * 60 * 1000);
   delay(sleepMins * 60 * 1000);
-  
+
   if (DEBUG) {
     Serial.println("Wake up");
   }
 }
 
-void sendString() {
+void sendTemp(float temp) {
   // Start the module
   SigFox.begin();
   SigFox.debug();
@@ -46,21 +65,21 @@ void sendString() {
   SigFox.status();
   delay(100);
 
-  int temp = SigFox.internalTemperature();
+  //  int temp = SigFox.internalTemperature();
   SigFox.beginPacket();
   SigFox.print(temp);
   int ret = SigFox.endPacket();  // send buffer to SIGFOX network
   SigFox.noDebug();
   SigFox.end();
-  
-  
+
+
   if (DEBUG) {
     if (ret > 0) {
       Serial.println("No transmission");
     } else {
       Serial.println("Transmission ok");
-      Serial.println(ret);
-      Serial.println(temp);
+      //      Serial.println(ret);
+      //      Serial.println(temp);
     }
   }
 
